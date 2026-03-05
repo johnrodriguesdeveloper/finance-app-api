8ac7e5c82625a4990b869b1135f2d76740c9e6db
John Rodrigues <johnrodrigues97106@gmail.com>
2026-03-05 09:32:49 -0300

    feat: created CreatedUserController

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import 'dotenv/config.js'
-
+import { CreateUserController } from './src/controllers/create-user.js';
 
 const app = express();
+
 app.use(express.json());
-app.listen(3000, () =>
-  console.log(`listening on port 3000`)
+
+app.post('/api/users',async (req, res) => {
+  const createUserController = new CreateUserController()
+  
+  const { statusCode,body} = await createUserController.execute(req)
+
+  res.status(statusCode).json(body)
+});
+
+app.listen(8080, () =>
+  console.log(`listening on port 8080`)
 );
