a863fc968a52e1096caf3a326581de419b6f52c5
John Rodrigues <johnrodrigues97106@gmail.com>
2026-03-07 14:33:32 -0300

    feat : update user

---
+++ src/repositories/postgres/update-user.js
@@ -0,0 +1,25 @@
+import { PostgresHelper } from "../../db/postgres/helper"
+
+
+export class UpdateUserRepository {
+  async execute(userId,updateParams) {
+      const updateFields = [] 
+      const updateValues = []
+
+      Object.keys(updateParams).forEach(key => {
+       updateFields.push(`${key} = $${updateFields.length + 1}`)
+       updateValues.push(updateParams[key]) 
+      })
+      updateValues.push(userId)
+
+      const updateQuery= `
+        UPDATE users 
+        SET ${updateFields.join(', ')} 
+        WHERE id = $${updateFields.length + 1}
+        RETURNING * 
+      `
+
+      const updateUser = await PostgresHelper.query(updateQuery, updateValues)
+      return updateUser[0]
+  }   
+}
