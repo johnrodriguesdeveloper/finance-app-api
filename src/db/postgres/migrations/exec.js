972058cd17e6f126fd3be65ad1b6a02ccc6fd3c3
John Rodrigues <johnrodrigues97106@gmail.com>
2026-05-07 16:17:56 -0300

    feat: add migrations and function data base get balance

--- src/db/postgres/migrations/exec.js
+++ src/db/postgres/migrations/exec.js
@@ -9,11 +9,22 @@ const __dirname = path.dirname(__filename)
 
 const execMigrations = async () => {
   const client = await pool.connect()
+
   try {
-    const filePath = path.join(__dirname, '01-init.sql')
-    const script = fs.readFileSync(filePath, 'utf-8')
-    await client.query(script)
-    console.log("Migration executed sucessfully.")
+    const files = fs
+      .readdirSync(__dirname)
+      .filter((file) => file.endsWith('.sql'))
+
+    for (const file of files) {
+      const filePath = path.join(__dirname, file)
+      const script = fs.readFileSync(filePath, 'utf-8')
+
+      await client.query(script)
+
+      console.log(`Migration for file ${file} executed successfully.`)
+    }
+
+    console.log('Migrations executed successfully.')
   } catch (error) {
     console.error(error)
   } finally {
