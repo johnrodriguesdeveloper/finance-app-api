d796f8e9b5d9bd5009f4897bfa8705dcf93e2988
John Rodrigues <johnrodrigues97106@gmail.com>
2026-05-11 17:47:53 -0300

    feat: ensure get user balance prisma

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,14 +1,46 @@
-import { PostgresHelper } from "../../../db/postgres/helper.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresGetUserBalanceRepository {
     async execute(userId) {
-        const result = await PostgresHelper.query(
-        `SELECT * FROM get_user_balance($1)`,
-          [userId]
-        )
-        return {
-          userId,
-          ...result[0]
-        }
+      const { _sum: { amount: totalExpenses } } = await prisma.transaction.aggregate({
+  where: {
+    user_id: userId,
+    type: 'EXPENSE',
+  },
+  _sum: {
+    amount: true,
+  },
+})
+
+const { _sum: { amount: totalEarnings } } = await prisma.transaction.aggregate({
+  where: {
+    user_id: userId,
+    type: 'EARNING',
+  },
+  _sum: {
+    amount: true,
+  },
+})
+
+const { _sum: { amount: totalInvestments } } = await prisma.transaction.aggregate({
+  where: {
+    user_id: userId,
+    type: 'INVESTMENT',
+  },
+  _sum: {
+    amount: true,
+  },
+})
+  const _totalEarning = totalEarnings || 0
+  const _totalExpense = totalExpenses || 0
+  const _totalInvestment = totalInvestments || 0
+
+const balance = _totalEarning - _totalExpense - _totalInvestment
+      return {
+        earnings: _totalEarning,
+        expenses: _totalExpense,
+        investments: _totalInvestment,
+        balance: balance
+      }
     }
 }
