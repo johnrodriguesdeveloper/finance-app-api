6697a90977cc8151d50d52b41cd5c7633e3829db
John Rodrigues <johnrodrigues97106@gmail.com>
2026-05-11 17:38:42 -0300

    feat: ensure prisma update, detele and get user by id

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -1,9 +1,13 @@
-import { PostgresHelper } from "../../../db/postgres/helper.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresDeleteUser {
   async execute(userId) {
-    const deletedUser = await PostgresHelper.query('DELETE FROM users WHERE id = $1 RETURNING *', [userId])
-    return deletedUser[0]
+    const deletedUser = await prisma.user.delete({
+      where: {
+        id: userId
+      }
+    })
+    return deletedUser
   }
   
 }
--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -1,25 +1,15 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 
 export class UpdateUserRepository {
   async execute(userId,updateParams) {
-      const updateFields = [] 
-      const updateValues = []
-
-      Object.keys(updateParams).forEach(key => {
-       updateFields.push(`${key} = $${updateFields.length + 1}`)
-       updateValues.push(updateParams[key]) 
-      })
-      updateValues.push(userId)
-
-      const updateQuery= `
-        UPDATE users 
-        SET ${updateFields.join(', ')} 
-        WHERE id = $${updateFields.length + 1}
-        RETURNING * 
-      `
-
-      const updateUser = await PostgresHelper.query(updateQuery, updateValues)
-      return updateUser[0]
+    const user = await prisma.user.update({
+      where: {
+        id: userId
+      },
+      data: updateParams
+    })
+   
+    return user
   }   
 }
