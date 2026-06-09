c4385a3256dbf12c9903f1baa95041b94fa0708d
John Rodrigues <johnrodrigues97106@gmail.com>
2026-06-09 17:39:03 -0300

    refactor : remove unused code

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,15 +1,9 @@
 
 
 import { badRequest, notFound } from "./http.js"
-import validator from "validator"
 
-export const invalidPasswordResponse = () => {
-  return badRequest({ message: 'Password must be at least 6 characters long' })
-}
 
-export const invalidEmailResponse = () => {
-  return badRequest({ message: 'Invalid email format' })
-}
+
 
 export const invalidIdRequiredResponse = () => {
   return badRequest({ message: 'User ID is required and must be a valid UUID' })
@@ -19,11 +13,6 @@ export const userNotFoundResponse = () => {
   return notFound({ message: 'User not found' })
 }
 
-export const checkIfPasswordIsValid = (password) => {
-  return password.length < 6
-}
 
-export const checkIfEmailIsValid = (email) => {
-  return !validator.isEmail(email)
-}
+
 
--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -5,36 +5,6 @@ export const checkIfIdIsValid = (id) => {
   return validator.isUUID(id)
 }
 
-export const requiredFieldResponse = (field) => {
-  return badRequest({ message: `The field ${field} is required` })
-}
-
 export const invalidIdResponse = () => {
   return badRequest({ message: 'The provided ID is not valid' })
-}
-
-export const checkIfIsString = (value) => {
-  return typeof value === 'string'
-}
-
-export const validateRequiredFields = (params, requiredFields) => {
-
-  for (const field of requiredFields) {
-
-    const fieldIsMissing = !params[field]
-    const fieldIsEmpty = checkIfIsString(params[field]) && validator.isEmpty(params[field],{
-      ignore_whitespace: true
-    })
-
-    if (fieldIsMissing || fieldIsEmpty) {
-      return {
-        field,
-        message: `Missing param: ${field}`,
-        ok: false
-      }
-    }
-  }
-  return { ok: true,
-    message: 'All required fields are present'
-   }
 }
